e040330d714a602057bc89628d0f272de826ac0e
jacobfed <jacobfed@umich.edu>
2018-10-20 18:43:58 -0400

    Working Login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -8,19 +8,6 @@ from app.models import User
 # ------------------------------------------
 #       Login/Logout/Register
 # ------------------------------------------
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('main.index'))
-    return render_template('auth/login.html', title='Sign In', form=form)
 
 @bp.route('/logout')
 def logout():
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request, current_app
-from flask_login import current_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from app import db
 from app.main.forms import PostForm
 from app.models import User, Post
 from app.main import bp
+from app.auth.forms import LoginForm, RegistrationForm
 
 # ------------------------------------------
 #                   User
@@ -57,24 +58,22 @@ def remove(id):
     object = Object.query.get_or_404(id)
     delete(object)
     return redirect(url_for('index'))
-# ------------------------------------------
-#       Login/Logout/Register
-# ------------------------------------------
+    
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+        return redirect(url_for('main.index'))
+    return render_template('auth/login.html', title='Sign In', form=form)
+
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-
+    return redirect(url_for('main.index'))
